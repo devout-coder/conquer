a0b6b3c7ce655ce64ddcdb38c67092dec6b350f7
pro <awessssomepro@gmail.com>
2020-12-22 21:25:02 +0530

    built year selector for month todos

--- src/Components/Month/Month.js
+++ src/Components/Month/Month.js
@@ -1,13 +1,54 @@
-import React from "react";
+import { FormControl, MenuItem, Select } from "@material-ui/core";
+import React, { useState } from "react";
 import Navbar from "../Navbar/Navbar";
 import Sidebar from "../Sidebar/Sidebar";
 import "./Month.css";
 
 function Month() {
+  const [year, setYear] = useState(new Date().getFullYear());
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  function getYears() {
+    let allYears = [];
+    let initialYear = 2020;
+    while (initialYear <= 2100) {
+      allYears.push(initialYear);
+      initialYear++;
+    }
+    return allYears;
+  }
   return (
     <div className="month">
       <Navbar />
-      <Sidebar />
+      <div className="monthPage">
+        <Sidebar />
+        <div className="monthPageEmpty">
+          <FormControl>
+            <Select
+              value={year}
+              onChange={(e) => setYear(e.target.value)}
+              displayEmpty
+              inputProps={{ "aria-label": "Without label" }}
+            >
+              {getYears().map((year) => (
+                <MenuItem value={year}>{year}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+      </div>
     </div>
   );
 }
