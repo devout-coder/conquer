cdb4b5238ab0104156b2ca9c7cbfbcae171032bd
pro <awessssomepro@gmail.com>
2020-12-25 21:43:07 +0530

    trying to push notifications for one more time

--- src/Components/Daily/Daily.js
+++ src/Components/Daily/Daily.js
@@ -10,8 +10,8 @@ function Daily() {
     title: "Conquer",
     subtitle: "This is a subtitle",
     message: "This is a very long message",
-    theme: "darkblue",
-    native: true, // when using native, your OS will handle theming.
+    // theme: "darkblue",
+    // native: true, // when using native, your OS will handle theming.
   });
   return (
     <div className="daily">
